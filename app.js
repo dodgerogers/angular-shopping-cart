4fc86c6af194078700140477525b5b0826f31748
Andy <dodgerogers@hotmail.com>
2014-08-10 18:48:47 -0400

    overwrite

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 function CartController($scope) {
 
 	$scope.bill = {};
-	$scope.items = [
-		{title: "Intense M1 Frameset", price: 2000.00},
-		{title: "Azonic DS1 Frameset", price: 300.00},
-		{title: "Marazocchi DJ1", price: 250.00},
-		{title: "Profile Cranks", price: 200.00},
-		{title: "Sun Ringle Hubs on Mavic 321's", price: 190.00},
-		{title: "North Shore Billet Derailleur Hanger", price: 25.00}
-	];
-	
 	$scope.shoppingCart = [];
+	$scope.items = [
+		{title: "Intense M1 Frameset", price: 2000.00, quantity: 1},
+		{title: "Azonic DS1 Frameset", price: 300.00, quantity: 1},
+		{title: "Marazocchi DJ1", price: 250.00, quantity: 1},
+		{title: "Profile Cranks", price: 200.00, quantity: 1},
+		{title: "Sun Ringle Hubs on Mavic 321's", price: 190.00, quantity: 1},
+		{title: "North Shore Billet Derailleur Hanger", price: 25.00, quantity: 1}
+	];
 	
 	$scope.subTotal = function(){
 		return $scope.totalCart() - $scope.bill.discount;
 	};
 	
 	$scope.totalCart = function(){
-		var total = 0, length = $scope.shoppingCart.length;
-		for(var i = 0; i < length; i++) {
-			total = total + $scope.shoppingCart[i].price * ($scope.shoppingCart[i].quantity || 0);
-		}
+		var total = 0;
+		angular.forEach($scope.shoppingCart, function(value, index){
+			total = total + value.price * (value.quantity || 0);
+		});
 		return total;
 	};
 	
@@ -30,20 +29,27 @@ function CartController($scope) {
 	
 	$scope.$watch($scope.totalCart, calculateDiscount);
 	
-	$scope.addToCart = function(item) {
-		// See if the item exists in the cart
-		var index = $scope.shoppingCart.indexOf(item);
-		
-		// Copy the item
+	$scope.addToCart = function(item){
 		var newItem = angular.copy(item);
+		var counter = 0;
 		
-		// push to the cart array
-		$scope.shoppingCart.push(newItem);
+		// updating an existing item quantity
+		angular.forEach($scope.shoppingCart, function(value, index) {
+			if(value.title === item.title){
+				value.quantity = parseInt(value.quantity) + parseInt(item.quantity);
+				counter++
+			}
+		});
+		
+		// if counter is still zero, it means nothing was updated
+		if(counter == 0) {
+			$scope.shoppingCart.push(newItem);
+		}
 	};
 	
-	$scope.removeFromCart = function(item) {
+	$scope.removeFromCart = function(item){
 		angular.forEach($scope.shoppingCart, function(value, index){
-			if(value.title === item.title) {
+			if(value.title === item.title){
 				$scope.shoppingCart.splice(index, 1);
 			}
 		});
